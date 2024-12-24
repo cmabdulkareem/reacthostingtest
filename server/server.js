bd09abd3c4429cfe5e06b336076cf3d307ab1251
“[abdul <[cmabdulkareem@gmail.com]>
2024-12-24 23:47:31 +0530

    initial

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = 3000;
 
 const corsOptions = {
-    origin : "http://localhost:5173",
+    origin : "https://reacthostingtest.vercel.app",
     methods : ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials : true,
     allowedHeaders: ["Content-Type", "Authorization"]
