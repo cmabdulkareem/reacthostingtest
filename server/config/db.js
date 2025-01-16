5bdcc460e66fd25d868d179f81a40116e3547086
“[abdul <[cmabdulkareem@gmail.com]>
2025-01-16 16:39:52 +0530

    initial

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://cadd:cadd123@<cluster-name>.mongodb.net/reactdb?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://kareemchala:Kareem262201@sample.kvxwkea.mongodb.net/cdcinsights")
     .then(()=>{console.log("connected to db")})
     .catch((err)=>{console.log("mongodb connection error", err)})
